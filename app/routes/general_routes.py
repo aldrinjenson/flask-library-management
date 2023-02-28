334592a6bcc2b360ffeabae3abc7de48e5e32f95
Aldrin Jenson <aldrinjenson@gmail.com>
2023-02-28 08:09:39 +0530

    404 page + minor formattings

--- app/routes/general_routes.py
+++ app/routes/general_routes.py
@@ -5,3 +5,8 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template('dashboard.html')
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
